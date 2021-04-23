fc3fd812cde531eec2db93225dd150e8983ed7d4
Nicolás Ruiz <nruiz@hexacta.com>
2021-04-23 00:34:30 -0300

    Added mysql hibernatedialect

--- src/test/java/ar/edu/unq/desapp/grupoa/backenddesappapi/BackendDesappApiApplicationTests.java
+++ src/test/java/ar/edu/unq/desapp/grupoa/backenddesappapi/BackendDesappApiApplicationTests.java
@@ -3,14 +3,11 @@ package ar.edu.unq.desapp.grupoa.backenddesappapi;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 @SpringBootTest
 class BackendDesappApiApplicationTests {
 
 	@Test
 	void contextLoads() {
-		assertTrue(true);
 	}
 
 }
