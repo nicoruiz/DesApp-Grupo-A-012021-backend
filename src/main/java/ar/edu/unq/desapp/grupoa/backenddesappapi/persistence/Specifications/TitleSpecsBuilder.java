d4364971d952dcc07f22b1369355d6a8134bd9f6
Nicolás Ruiz <nruiz@hexacta.com>
2021-06-06 12:55:53 -0300

    Fix decade filter pair

--- src/main/java/ar/edu/unq/desapp/grupoa/backenddesappapi/persistence/Specifications/TitleSpecsBuilder.java
+++ src/main/java/ar/edu/unq/desapp/grupoa/backenddesappapi/persistence/Specifications/TitleSpecsBuilder.java
@@ -2,18 +2,18 @@ package ar.edu.unq.desapp.grupoa.backenddesappapi.persistence.Specifications;
 
 import ar.edu.unq.desapp.grupoa.backenddesappapi.controllers.dtos.titles.SearchTitleParamsDto;
 import ar.edu.unq.desapp.grupoa.backenddesappapi.model.Title;
-import javafx.util.Pair;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Component;
 
 import javax.persistence.criteria.JoinType;
+import java.util.AbstractMap;
 import java.util.HashMap;
 import java.util.Map;
 
 @Component
 public class TitleSpecsBuilder implements SpecBuilder<Title, SearchTitleParamsDto> {
     private final double TOP_RATED_VALUE = 4.5;
-    private final Map<Integer, Pair<Integer, Integer>> DECADES_DICT = this.initDecadesDict();
+    private final Map<Integer, AbstractMap.SimpleEntry<Integer, Integer>> DECADES_DICT = this.initDecadesDict();
 
     @Override
     public Specification<Title> buildCriteriaSpecs(SearchTitleParamsDto params) {
@@ -39,7 +39,7 @@ public class TitleSpecsBuilder implements SpecBuilder<Title, SearchTitleParamsDt
     private Specification<Title> withDecade(Integer decade) {
         if (decade == null) return null;
 
-        Pair<Integer, Integer> decadePair = DECADES_DICT.getOrDefault(decade, null);
+        AbstractMap.SimpleEntry<Integer, Integer> decadePair = DECADES_DICT.getOrDefault(decade, null);
 
         return (root, query, cb) -> decadePair == null ? null :
                 cb.between(root.get("startYear"), decadePair.getKey(), decadePair.getValue());
@@ -64,14 +64,14 @@ public class TitleSpecsBuilder implements SpecBuilder<Title, SearchTitleParamsDt
         };
     }
 
-    private Map<Integer, Pair<Integer, Integer>> initDecadesDict() {
-        Map<Integer, Pair<Integer, Integer>> decades = new HashMap<>();
+    private Map<Integer, AbstractMap.SimpleEntry<Integer, Integer>> initDecadesDict() {
+        Map<Integer, AbstractMap.SimpleEntry<Integer, Integer>> decades = new HashMap<>();
         int startDecade = 1890;
         int endDecade = 2020;
 
         for (int i = startDecade; i <= endDecade; i = i+10) {
             int endYear = i+9;
-            decades.put(i, new Pair<>(i, endYear));
+            decades.put(i, new AbstractMap.SimpleEntry<>(i, endYear));
         }
 
         return decades;
